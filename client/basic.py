78064888fe185da5c4fd1ab3c2da826adad9f80b
dauntless <suntopmultimedia@gmail.com>
2022-03-31 00:52:59 -0700

    client

--- client/basic.py
+++ client/basic.py
@@ -1,10 +1,9 @@
+'''
+Test Api Via the console with the client, add the necessary parameters and url to use
+'''
+
 import requests
 from getpass import getpass
-
-import base64
-
-
-
 
 username = input('Enter username: ')
 password = getpass('Enter Password: ')
@@ -22,14 +21,9 @@ headers = {
 post_url = "http://127.0.0.1:8000/posts/"
 
 
-
-files = "C:/Users/user/Desktop/abnet/abnet/assets/img/portfolio/portfolio-details-2.jpg"
-
-
-
 post_response = requests.post(post_url, headers=headers, json={
     "author": 1, "title": "We made it hard", "content": "success", "slug": "hollan","category": None,
     "status": "published"
 })
 
-# print(post_response.json())
+print(post_response.json())
